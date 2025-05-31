c3f09b13e60ef4f2f9405853310ce0a546d89cc2
jonme3 <jonme3@hotmail.com>
2025-05-31 15:15:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def list_static():
     return jsonify(files)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
